841283c9aa7f568859a4c838303d8ca101886a28
Tobias Müller <tobimandroid@googlemail.com>
2021-08-23 12:38:03 +0200

    new approach

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
-function setup() {
-  var cnv = createCanvas(1400, 750)
+var cnv;
+
+function centerCanvas() {
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
+}
+
+function setup() {
+  cnv = createCanvas(windowWidth - width, windowHeight - height);
+  centerCanvas();
   background(255, 0, 200);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  centerCanvas();
 }
